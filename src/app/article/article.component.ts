35547f1ac8d7fcb90678552265198a24607cda8b
igoryen <igor.yentaltsev@gmail.com>
2016-12-28 07:17:34 +0000

    Component Article is displayed

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -3,11 +3,27 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
-  styleUrls: ['./article.component.css']
+  styleUrls: ['./article.component.css'],
+  host: { class: "row" } // 5
 })
 export class ArticleComponent implements OnInit {
+    votes: number; // 6
+    title: string; // 7
+    link: string; // 8
 
-  constructor() { }
+  constructor() {
+    this.title = "Angular 2";
+    this.link = "http//angular.io";
+    this.votes = 10;
+  }
+
+  voteUp() {
+    this.votes++;
+  }
+
+  voteDown() {
+    this.votes--;
+  }
 
   ngOnInit() {
   }

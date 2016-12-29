09acfd38da55e331d128cf775d8083158532ad60
igoryen <igor.yentaltsev@gmail.com>
2016-12-29 23:13:33 +0000

    Can sort articles based on votes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,4 +27,8 @@ export class AppComponent {
         link.value = ''; // 27
         return false;
     }
+
+    sortedArticles(): Article[] {
+        return this.articles.sort( (a: Article, b: Article) => b.votes - a.votes ); // 29
+    }
 }
